3b0cba47e154eccecf35ef67528193c0290e8362
rungefuk <roman.ungefuk@gmail.com>
2021-03-25 02:48:00 +0300

    add exception handling to project response as json format

--- src/main/java/com/example/customexception/CustomException.java
+++ src/main/java/com/example/customexception/CustomException.java
@@ -3,6 +3,7 @@ package com.example.customexception;
 public class CustomException extends Exception {
 
     public CustomException() {
+
         super("my own exception");
     }
 }
--- src/main/java/com/example/customexception/MyExceptionHandlers.java
+++ src/main/java/com/example/customexception/MyExceptionHandlers.java
@@ -1,14 +1,21 @@
 package com.example.customexception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class MyExceptionHandlers {
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<Object> myMessage(CustomException ce) {
-        return new ResponseEntity<>(ce.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+public class MyExceptionHandlers extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CustomException.class )
+    public ResponseEntity<Object> handleAll(final CustomException ce) {
+        logger.info(ce.getClass().getName());
+        logger.error("error", ce);
+
+        final ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ce.getLocalizedMessage(), "error occurred");
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
 }
